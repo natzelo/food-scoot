7ea40e55ae343ef514cdc631a3872f03288482f3
ryuzaki221 <52002984+ryuzaki221@users.noreply.github.com>
2021-06-22 15:27:05 +0530

    custom menu for each restaurant

--- app/src/main/java/com/example/foodbunny/fragment/FragmentRestaurantMenu.kt
+++ app/src/main/java/com/example/foodbunny/fragment/FragmentRestaurantMenu.kt
@@ -9,17 +9,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.foodbunny.adaptor.RestaurantMenuRecyclerAdapter
 import com.example.foodbunny.databinding.FragmentRestaurantMenuBinding
 import com.example.foodbunny.model.Food
+import org.json.JSONException
 
 class FragmentRestaurantMenu : Fragment() {
 
-    private val foods = arrayListOf(
-        Food("Potato", "200"),
-        Food("Chicken Bhuna", "400"),
-        Food("Cheese", "120")
-    )
+    private val foods: ArrayList<Food> = arrayListOf()
 
     private lateinit var  binding: FragmentRestaurantMenuBinding
     override fun onCreateView(
@@ -28,17 +29,58 @@ class FragmentRestaurantMenu : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentRestaurantMenuBinding.inflate(layoutInflater)
+        binding.restaurantMenuProgressLayout.visibility = View.VISIBLE
         val bundle:Bundle? = arguments
         Log.i("IN TRACK", "bundle ${bundle.toString()}")
         val restaurantId = bundle?.getString("restaurant_id")
         Toast.makeText(activity as Context, "Restaurant Id is $restaurantId", Toast.LENGTH_SHORT).show()
-
         val layoutManager = LinearLayoutManager(activity)
-        val restaurantMenuAdapter = RestaurantMenuRecyclerAdapter(activity as Context, foods)
+        val queue = Volley.newRequestQueue(activity as Context)
+        val url = "http://13.235.250.119/v2/restaurants/fetch_result/$restaurantId"
 
-        binding.restaurantMenuRecycler.layoutManager = layoutManager
-        binding.restaurantMenuRecycler.adapter = restaurantMenuAdapter
+        val jsonObjectRequest = object : JsonObjectRequest(Request.Method.GET, url, null, Response.Listener {
+            try {
+                val data = it.getJSONObject("data")
+                val success = data.getBoolean("success")
+                if(success) {
+                    binding.restaurantMenuProgressLayout.visibility = View.GONE
+                    val dataArray = data.getJSONArray("data")
 
+                    for(i in 0 until dataArray.length()) {
+                        val foodJSON = dataArray.getJSONObject(i)
+
+                        val food = Food(
+                            foodJSON.getString("name"),
+                            foodJSON.getString("cost_for_one")
+                        )
+
+                        foods.add(food)
+                        val restaurantMenuAdapter = RestaurantMenuRecyclerAdapter(activity as Context, foods)
+
+                        binding.restaurantMenuRecycler.layoutManager = layoutManager
+                        binding.restaurantMenuRecycler.adapter = restaurantMenuAdapter
+                    }
+                } else {
+                    Toast.makeText(activity as Context,"Some Error Occurred", Toast.LENGTH_SHORT).show()
+                }
+            } catch (e: JSONException) {
+                if(activity != null) {
+                    Toast.makeText(activity, "Some Error", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }, Response.ErrorListener {
+            if(activity != null) {
+                Toast.makeText(activity, "Volley Error", Toast.LENGTH_SHORT).show()
+            }
+        }) {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Content-Type"] = "application/json"
+                headers["token"] = "5fc57ce44a41aa"
+                return headers
+            }
+        }
+        queue.add(jsonObjectRequest)
         return binding.root
     }
 
